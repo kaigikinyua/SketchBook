615479a8c9257155279ddd1a4991aeed8e68cd36
kaigikinyua <kaigikinyua@gmail.com>
2019-01-27 20:58:15 +0300

    adding filters to account for the file shift

--- DirSort/filter.py
+++ DirSort/filter.py
@@ -1,1 +1,25 @@
 #used to filter the data entered into the program
+import os
+class Filter():
+    def checkDir(self,dir):
+        if(os.path.isdir(dir)==True):
+            return True
+        else:
+            return False
+    
+    def enterDir(self):
+        dir=raw_input("Enter Directory\n")
+        return dir
+    
+    def extension(self,ext):
+        #move the extension to the directory
+        print("Hello")
+    def history(self,destination,former):
+        d=os.listdir(destination)
+        f=open("destination","w")
+        f.write(d)
+        f.close()
+        f=open("former","w")
+        f.write(os.listdir(former))
+        f.close()
+
