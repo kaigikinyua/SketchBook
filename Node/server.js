621489f28122fb6cf0092554b61359433dd1a7e6
kaigikinyua <kaigikinyua@gmail.com>
2019-01-29 22:45:05 +0300

    routing

--- Node/server.js
+++ Node/server.js
@@ -1,12 +1,20 @@
 var http=require('http')
 var url=require('url')
 var fs=require('fs')
+var express=require('express')
 var server=http.createServer(function (req,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
-	var path=url.parse(req.url).pathname;
-	console.log("Here is the path "+path);
-	var read=fs.createReadStream(__dirname+'/index.html','utf8');
-	read.pipe(res)
+	console.log("Here is the path "+req.url);
+	if (req.url=='/apps'){
+		var read=fs.createReadStream(__dirname+'/apps.html','utf8');
+		read.pipe(res);
+	}else if(req.url=='/'){
+		var read=fs.createReadStream(__dirname+'/index.html','utf8');
+		read.pipe(res);
+	}else{
+		res.writeHead(200,{'Content-Type':'text/html'});
+		res.end('Error');
+	}
 }
 );
 server.listen(3000,'127.0.0.1');
