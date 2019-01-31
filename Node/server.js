818636782993b26435f77cb662f3be78beebe23b
kaigikinyua <kaigikinyua@gmail.com>
2019-01-31 22:31:06 +0300

    json

--- Node/server.js
+++ Node/server.js
@@ -1,21 +1,24 @@
 var http=require('http')
 var url=require('url')
 var fs=require('fs')
-var express=require('express')
 var server=http.createServer(function (req,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
 	console.log("Here is the path "+req.url);
-	if (req.url=='/apps'){
-		var read=fs.createReadStream(__dirname+'/apps.html','utf8');
-		read.pipe(res);
-	}else if(req.url=='/'){
-		var read=fs.createReadStream(__dirname+'/index.html','utf8');
-		read.pipe(res);
-	}else{
+	if(req.url=='/apps'){
 		res.writeHead(200,{'Content-Type':'text/html'});
-		res.end('Error');
+		var myRes=fs.createReadStream(__dirname+'/apps.html','utf8');
+		myRes.pipe(res);
+	}
+	else if(req.url=='/json'){
+		var myJson={name:'Kraken',type:'1964 Ford',use:'DragStar'};
+		res.writeHead(200,{'Content-Type':'application/json'});
+		res.end(JSON.stringify(myJson));
+	}
+	else{
+		res.writeHead(200,{'Content-Type':'text/html'});
+		var myRes=fs.createReadStream(__dirname+'/index.html','utf8');
+		myRes.pipe(res);
 	}
 }
-);
-server.listen(3000,'127.0.0.1');
+).listen(3000,'127.0.0.1');
 console.log('Server Running at port 3000');
