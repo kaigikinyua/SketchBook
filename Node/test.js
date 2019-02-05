7bf94560a030a70b03b4bc93fa3a5a830885c398
kaigikinyua <kaigikinyua@gmail.com>
2019-02-05 07:54:48 +0300

    Using EJS

--- Node/test.js
+++ Node/test.js
@@ -1,10 +1,14 @@
 var express=require('express')
 var app=express()
+var bodyparser=require('body-parser')
+var urlencoded=bodyparser.urlencoded({extended:false});
 app.set('view engine','ejs');
-app.use('/apps',function(res,req,next){
-    next();
+app.use('/index',express.static('static'));
+app.get('/index',function (req,res){
+    res.render('index');
 });
-app.get('/apps',function (req,res){
+
+app.get('/index/apps',function (req,res){
     res.render('basic');
 })
 app.listen(3001);
