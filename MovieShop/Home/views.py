82cb65238d7937ede3f3843560cdcc4b19038d0a
kaigikinyua <kaigikinyua@gmail.com>
2019-03-20 23:01:22 +0300

    Movie Shop

---
+++ MovieShop/Home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+	return render(request,'Home/home.html')
+def trailers(request):
+	return render(request,'Home/trailers.html')
