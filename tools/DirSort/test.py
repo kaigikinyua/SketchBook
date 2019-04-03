aea2fde65478e136d4b57c07163aa69a1f266e2f
kaigikinyua <kaigikinyua@gmail.com>
2019-04-03 16:45:44 +0300

    -----Rock meet Bottom----

---
+++ tools/DirSort/test.py
@@ -0,0 +1,6 @@
+class Test():
+	def __init__(self):
+		print("Moving to Django and Python 3")
+		print("Teach Myself python lists,slices,dictionaries and JSON")
+		print("Teach Myself Inheritance In python")
+		print("Teach myself using sqlite3")
